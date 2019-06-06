f1af8134a01580275071ce3a94018dc7f19b2c02
sheetanshk <49148845+sheetanshk@users.noreply.github.com>
2019-06-06 12:21:01 +0530

    Add files via upload

---
+++ Test/src/main/java/Sample_mobile_Automation/Test/App.java
@@ -0,0 +1,13 @@
+package Sample_mobile_Automation.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
